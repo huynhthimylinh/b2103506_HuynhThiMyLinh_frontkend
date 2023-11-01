6959c8d3bdbc972f3b314caecb3390206f2b2c86
huynhthimylinh <huynhthimylinh55@gmail.com>
2023-11-01 09:41:18 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -16,13 +16,13 @@
       <p v-else>Không có liên hệ nào.</p>
       <div class="mt-3 row justify-content-around align-items-center">
         <button class="btn btn-sm btn-primary" @click="refreshList()">
-          <i class="fas fa-redo"></i> Làm mới
+          <i class="fab fa-dyalog"></i> Làm mới
         </button>
         <button class="btn btn-sm btn-success" @click="goToAddContact">
-          <i class="fas fa-plus"></i> Thêm mới
+          <i class="fa fa-plus"></i> Thêm mới
         </button>
         <button class="btn btn-sm btn-danger" @click="removeAllContacts">
-          <i class="fas fa-trash"></i> Xóa tất cả
+          <i class="fas fa-trash-alt"></i> Xóa tất cả
         </button>
       </div>
     </div>
